f046cd374e34e80f3739cbda301b799333f52d5e
Lassi Kortela <lassi@lassi.io>
2019-12-28 02:49:39 +0200

    Add Gauche test

--- test.sh
+++ test.sh
@@ -6,6 +6,7 @@ payload="trivial-tar-writer.scm test.sh"
 set -x
 chibi-scheme -A . test-r7rs.scm $payload >test-chibi.tar
 gsi-script . test-r7rs.scm $payload >test-gambit.tar
+gosh -A . test-r7rs.scm $payload >test-gauche.tar
 bsdtar -cf test-bsd.tar $payload
 gtar -cf test-gnu.tar $payload
 hexdump -C test-chibi.tar
